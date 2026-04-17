da1715bf13965499e092982654ea0bfa1b6e32f7
Amorim <josamorim@borgwarner.com>
2026-04-17 16:01:21 +0100

    RMS Maintenance V1

--- fix_line57.py
+++
@@ -1,6 +0,0 @@
-with open('C:/Users/josamorim/RMS-Maintenance/templates/projects.html', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-lines[56] = '                        <button class="action-btn edit-btn me-2" title="Editar" data-id="{{ project.id }}" data-name="{{ project.name }}" data-desc="{{ project.description }}" data-resp="{{ project.responsible_list|tojson }}" onclick="openEditProject(this)">\n'
-with open('C:/Users/josamorim/RMS-Maintenance/templates/projects.html', 'w', encoding='utf-8') as f:
-    f.writelines(lines)
-print('done')
